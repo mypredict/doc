6ef9ccc7dd9193242ff3615d797b1a1cf9f99650
guolj <mypredicts@gmail.com>
2024-01-20 22:39:04 +0800

    feat

--- 代理白名单.js
+++ 代理白名单.js
@@ -1,1 +1,1 @@
-169.254/16,*.local,*.todobox.vip,*.qlapp.vip,*.a11a22.com,*.a88a99.com,*.songminju.com,*.cocos.org,*.antgroup.com,*.iconfont.cn,*.zhihu.com,*.baidu.com,*.cn,*.tencent.com,*.cnblogs.com
+169.254/16,*local,*todobox.vip,*qlapp.vip,*a11a22.com,*a88a99.com,*songminju.com,*cocos.org,*antgroup.com,*iconfont.cn,*zhihu.com,*baidu.com,*cn,*tencent.com,*cnblogs.com,*qq.com
